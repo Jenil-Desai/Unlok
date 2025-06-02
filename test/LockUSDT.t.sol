7ca8cb5063a7f9be6342c54d9113c958f5a75bb5
Jenil Desai <jenildev91@gmail.com>
2025-06-02 20:07:26 +0530

    v1.0.1
    + Added Openzepplin Contracts.
    + Added Basic Stucture.

---
+++ test/LockUSDT.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {LockUSDT} from "../src/LockUSDT.sol";
+
+contract LockUSDTTest is Test {
+    LockUSDT public lockUSDT;
+}
