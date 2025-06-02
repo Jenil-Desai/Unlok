2321d9e988ce7a2a4d5da3aff6be3addab18c88f
Jenil Desai <jenildev91@gmail.com>
2025-06-02 21:17:49 +0530

    v1.2.2
    + Fixed Linting Issues.

--- src/Lock.sol
+++ src/Lock.sol
@@ -5,11 +5,11 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Lock is Ownable {
-    mapping(address => mapping(IERC20 => uint)) private lockedBalances;
+    mapping(address => mapping(IERC20 => uint256)) private lockedBalances;
 
     constructor() Ownable(msg.sender) {}
 
-    function deposit(IERC20 _tokenaddress, uint _amount) public returns (bool) {
+    function deposit(IERC20 _tokenaddress, uint256 _amount) public returns (bool) {
         require(_amount > 0, "Amount must be greater than zero");
         require(_tokenaddress.allowance(msg.sender, address(this)) >= _amount);
         require(_tokenaddress.transferFrom(msg.sender, address(this), _amount));
@@ -17,15 +17,9 @@ contract Lock is Ownable {
         return true;
     }
 
-    function withdraw(
-        IERC20 _tokenaddress,
-        uint _amount
-    ) public returns (bool) {
+    function withdraw(IERC20 _tokenaddress, uint256 _amount) public returns (bool) {
         require(_amount > 0, "Amount must be greater than zero");
-        require(
-            lockedBalances[msg.sender][_tokenaddress] >= _amount,
-            "Insufficient balance"
-        );
+        require(lockedBalances[msg.sender][_tokenaddress] >= _amount, "Insufficient balance");
         lockedBalances[msg.sender][_tokenaddress] -= _amount;
         require(_tokenaddress.transfer(msg.sender, _amount), "Transfer failed");
         return true;
--- test/LockUSDT.t.sol
+++ test/LockUSDT.t.sol
@@ -1,32 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
-import {Lock} from "../src/Lock.sol";
-import {USDT} from "../src/USDT.sol";
+import { Test } from "forge-std/Test.sol";
+import { Lock } from "../src/Lock.sol";
+import { USDT } from "../src/USDT.sol";
 
 contract LockUSDTTest is Test {
-    USDT public usdt;
-    Lock public lock;
+  USDT public usdt;
+  Lock public lock;
 
-    function setUp() public {
-        usdt = new USDT();
-        lock = new Lock();
-    }
+  function setUp() public {
+    usdt = new USDT();
+    lock = new Lock();
+  }
 
-    function test_Deposit() public {
-        usdt.mint(0x015a239874606A99Edb2dcAB3025fBCd286731b4, 500);
-        vm.startPrank(0x015a239874606A99Edb2dcAB3025fBCd286731b4);
+  function test_Deposit() public {
+    usdt.mint(0x015a239874606A99Edb2dcAB3025fBCd286731b4, 500);
+    vm.startPrank(0x015a239874606A99Edb2dcAB3025fBCd286731b4);
 
-        usdt.approve(address(lock), 500);
-        lock.deposit(usdt, 500);
-        assertEq(lock.balance(usdt), 500);
+    usdt.approve(address(lock), 500);
+    lock.deposit(usdt, 500);
+    assertEq(lock.balance(usdt), 500);
 
-        lock.withdraw(usdt, 300);
-        assertEq(
-            usdt.balanceOf(0x015a239874606A99Edb2dcAB3025fBCd286731b4),
-            300
-        );
-        assertEq(usdt.balanceOf(address(lock)), 200);
-    }
+    lock.withdraw(usdt, 300);
+    assertEq(usdt.balanceOf(0x015a239874606A99Edb2dcAB3025fBCd286731b4), 300);
+    assertEq(usdt.balanceOf(address(lock)), 200);
+  }
 }
